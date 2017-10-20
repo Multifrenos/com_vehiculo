ad93d2a5b11b4998a9bf9d0e29bd0e90f8efbeac
Ricardo Oficina <ricardolau6@gmail.com>
2017-10-20 10:22:39 +0200

    Añado en vista cruces versiones con virtuemart, la celdas de las tablas para mostrar: - Nombre Producto - Nombre Versions ( Marca-Modelo-Version) 	modified:   admin/languages/es-ES/es-ES.com_vehiculo.ini 	modified:   admin/views/virtuemartcruces/tmpl/default_head.php

--- admin/views/virtuemartcruces/tmpl/default_head.php
+++ admin/views/virtuemartcruces/tmpl/default_head.php
@@ -5,15 +5,23 @@ defined('_JEXEC') or die('Restricted Access');
 
 <tr>
         <th width="5">
+                <?php echo JText::_('COM_VEHICULO_CRUCE_VIRTUEMART_HEADING_SELECCIONAR'); ?>
+        </th>
+        <th width="5">
                 <?php echo JText::_('COM_VEHICULO_CRUCE_VIRTUEMART_HEADING_PRODUCT'); ?>
         </th>
         <th width="5">
                 <?php echo JText::_('COM_VEHICULO_CRUCE_VIRTUEMART_HEADING_VERSION_VEHICULO'); ?>
         </th>
 		<th width="5">
-                <?php echo JText::_('COM_VEHICULO_CRUCE_VIRTUEMART_HEADING_FECHA_ACTUALIZACION'); ?>
+                <?php echo JText::_('COM_VEHICULO_CRUCE_VIRTUEMART_HEADING_NOMBRE_PRODUCT'); ?>
         </th>
         <th width="5">
-                <?php echo JText::_('COM_VEHICULO_CRUCE_VIRTUEMART_HEADING_SELECCIONAR'); ?>
+                <?php echo JText::_('COM_VEHICULO_CRUCE_VIRTUEMART_HEADING_NOMBRE_VERSIONS'); ?>
         </th>
+		
+		<th width="5">
+                <?php echo JText::_('COM_VEHICULO_CRUCE_VIRTUEMART_HEADING_FECHA_ACTUALIZACION'); ?>
+        </th>
+        
 </tr>
