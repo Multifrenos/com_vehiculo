15553596eaaafb3f5b900f01a11ac322e3455a94
Antonio-Multifrenos <multifrenos.es@gmail.com>
2017-10-25 18:39:08 +0200

    Primeros cambios para poder poner filtro y buscar en la vista nodelos.  Changes to be committed: 	modified:   admin/models/forms/filter_nodelos.xml 	modified:   admin/models/nodelos.php 	modified:   admin/views/nodelos/view.html.php

--- admin/models/nodelos.php
+++ admin/models/nodelos.php
@@ -2,12 +2,38 @@
 // No permitir el acceso directo al archivo
 defined('_JEXEC') or die('Restricted access');
 // Importar biblioteca modellist de Joomla
-//~ jimport('joomla.application.component.modellist');
+jimport('joomla.application.component.modellist');
 /**
  * NodeloList Model
  */
 class VehiculoModelNodelos extends JModelList
 {
+        
+        
+        	/**
+	 * Constructor.
+	 *
+	 * @param   array  An optional associative array of configuration settings.
+	 * @see     JController
+	 * @since   1.6
+	 */
+	public function __construct($config = array())
+	{
+		if (empty($config['filter_fields']))
+		{
+			$config['filter_fields'] = array(
+				'id', 'mo.id',
+                   	'marca', 'ma.nombre',
+                   'modelo', 'mo.nombre',
+                                
+			);
+		}
+
+		parent::__construct($config);
+	}       
+        
+        
+        
         /**
          * Método para crear una consulta SQL para cargar los datos de la lista.
          *
@@ -15,12 +41,59 @@ class VehiculoModelNodelos extends JModelList
          */
         protected function getListQuery()
         {
-                // Cree un objeto de consulta nueva.           
+                  // Build the query
                 $db = JFactory::getDBO();
-                $query = $db->getQuery(true);
-                // Seleccione algunos campos
-                $query->select('id,idMarca,nombre');
-                $query->from('#__vehiculo_modelos');
+                $query = $db->getQuery(true)
+                        ->select('mo.*')
+                        ->select ('ma.nombre as marca')
+                        ->from('#__vehiculo_modelos as mo')
+                        ->join('left', '#__vehiculo_marcas as ma ON (mo.idMarca = ma.id)')
+                
+
+		// Filter by search in title
+		$search = $this->getState('filter.search');
+		if (!empty($search))
+		{
+			if (stripos($search, 'id:') === 0)
+			{
+                                // Search id:1234
+				$query->where('mo.id = ' . (int) substr($search, 3));
+			}
+			else
+			{
+                                // Search %abcd%
+				$search = $db->quote('%' . str_replace(' ', '%', $db->escape(trim($search), true) . '%'));
+				$query->where('(mo.nombre LIKE ' . $search . ')');
+			}
+		}                    
+
+                
+                // Marca filter                
+                $idMarca = $this->getState('filter.idMarca');
+                if (is_numeric($idMarca)) {
+                       $query->where('ma.id = ' . (int) $idMarca); 
+                }                
+                
+                // Modelo filter                
+                $idModelo = $this->getState('filter.idModelo');
+                if (is_numeric($idModelo)) {
+                       $query->where('mo.id = ' . (int) $idModelo); 
+                }                                
+                
+                // Add the list ordering clause.                
+                $listOrder = $this->getState('list.ordering', 'mo.id');                
+				$listDirn = $db->escape($this->getState('list.direction', 'DESC'));                                                
+                $query->order($db->escape($listOrder) . ' ' . $listDirn);                
+                
                 return $query;
+               
+              
+                //~ // Cree un objeto de consulta nueva.           
+                //~ $db = JFactory::getDBO();
+                //~ $query = $db->getQuery(true);
+                //~ // Seleccione algunos campos
+                //~ $query->select('id,idMarca,nombre');
+                //~ $query->from('#__vehiculo_modelos');
+                //~ return $query;
         }
 }
--- admin/views/nodelos/view.html.php
+++ admin/views/nodelos/view.html.php
@@ -13,40 +13,74 @@ class VehiculoViewNodelos extends JViewLegacy
          *Métodoo para mostrar la vista Nodelos 
          * @return void
          */
-        function display($tpl = null) 
-        {
-                $basedatos = JFactory::getDBO();
-                $consulta = $basedatos->getQuery(true);
-                $consulta->select('id,nombre,logo');
-                $consulta->from('#__vehiculo_marcas');
-
-                $basedatos->setQuery((string)$consulta);
-                $resultado = $basedatos->loadObjectList();
+          $this->state            = $this->get('State');
+                $this->filterForm       = $this->get('FilterForm');
+                $this->activeFilters    = $this->get('ActiveFilters');
+                $items                  = $this->get('Items');
+                $pagination             = $this->get('Pagination');               
                 
-                // Obtener los datos desde el modelo
-                $items = $this->get('Items');
-                $pagination = $this->get('Pagination');
-
-                // Verificar existencia de errores.
-                if (count($errors = $this->get('Errors'))) 
-                {
-                        JError::raiseError(500, implode('<br />', $errors));
-                        return false;
-                }
-                // Asignar los datos a la vista
-                $this->items = $items;
-                $this->todasMarcas =$resultado;
-                $this->pagination = $pagination;
-				/* Cargamos Submenu y con el parametro 'nodelos' indicamos que está seleccionada*/
-				VehiculoHelper::addSubmenu('nodelos');
+		// Check for errors.
+		if (count($errors = $this->get('Errors'))) 
+		{
+			JError::raiseError(500, implode('<br />', $errors));
+			return false;
+		}
+                
+		// Assign data to the view
+		$this->items = $items;                
+		$this->pagination = $pagination;                
+                
+                
+                /* Cargamos Submenu y con el parametro 'versiones' indicamos que está seleccionada*/
+                VehiculoHelper::addSubmenu('nodelos');
 
                 // Establecer la barra de herramientas
                 $this->addToolBar();
                 
                 // Mostrar la plantilla
-                parent::display($tpl);
+                parent::display($tpl);                                
         }
         
+        /**
+         
+         
+         
+         
+         
+        //~ function display($tpl = null) 
+        //~ {
+                //~ $basedatos = JFactory::getDBO();
+                //~ $consulta = $basedatos->getQuery(true);
+                //~ $consulta->select('id,nombre,logo');
+                //~ $consulta->from('#__vehiculo_marcas');
+//~ 
+                //~ $basedatos->setQuery((string)$consulta);
+                //~ $resultado = $basedatos->loadObjectList();
+                //~ 
+                //~ // Obtener los datos desde el modelo
+                //~ $items = $this->get('Items');
+                //~ $pagination = $this->get('Pagination');
+//~ 
+                //~ // Verificar existencia de errores.
+                //~ if (count($errors = $this->get('Errors'))) 
+                //~ {
+                        //~ JError::raiseError(500, implode('<br />', $errors));
+                        //~ return false;
+                //~ }
+                //~ // Asignar los datos a la vista
+                //~ $this->items = $items;
+                //~ $this->todasMarcas =$resultado;
+                //~ $this->pagination = $pagination;
+				//~ /* Cargamos Submenu y con el parametro 'nodelos' indicamos que está seleccionada*/
+				//~ VehiculoHelper::addSubmenu('nodelos');
+//~ 
+                //~ // Establecer la barra de herramientas
+                //~ $this->addToolBar();
+                //~ 
+                //~ // Mostrar la plantilla
+                //~ parent::display($tpl);
+        //~ }
+        //~ 
         /**
          * Configurar la barra de herramientas
          */
