27f292a87519972a5b4bceac55ebb1a892e82d03
Antonio-Multifrenos <multifrenos.es@gmail.com>
2017-11-07 19:14:36 +0100

    - Soluciono la vista virtuemart_cruces 	new file:   admin/models/forms/filter_virtuemartcruces.xml 	modified:   admin/views/virtuemartcruces/tmpl/default.php

--- admin/views/virtuemartcruces/tmpl/default.php
+++ admin/views/virtuemartcruces/tmpl/default.php
@@ -7,18 +7,35 @@ defined('_JEXEC') or die;
 JHtml::_('bootstrap.tooltip');
 JHtml::_('behavior.multiselect');
 JHtml::_('formbehavior.chosen', 'select');
+$listOrder      = $this->escape($this->state->get('list.ordering', 'cr.id'));
+$listDirn       = $this->escape($this->state->get('list.direction', 'DESC'));
 ?>
 <form action="<?php echo JRoute::_('index.php?option=com_vehiculo'); ?>" method="post" name="adminForm"  id="adminForm">
+         <?php
+        // Search tools bar
+        echo JLayoutHelper::render('joomla.searchtools.default', array('view' => $this));
+        ?>
+                    
+        <?php if (empty($this->items)) : ?>
+                
+                <div class="alert alert-no-items">
+                        <?php echo JText::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
+                </div>
+                    
+        <?php else : ?>      
+        
+        
+        
         <table class="table table-striped">
                 <thead><?php echo $this->loadTemplate('head');?></thead>
                 <tfoot><?php echo $this->loadTemplate('foot');?></tfoot>
                 <tbody><?php echo $this->loadTemplate('body');?></tbody>
         </table>
-        <div>
-                <input type="hidden" name="task" value="" />
-                <input type="hidden" name="boxchecked" value="0" />
-                <?php echo JHtml::_('form.token'); ?>
-        </div>
+        <?php endif; ?>
+
+        <input type="hidden" name="task" value="" />
+        <input type="hidden" name="boxchecked" value="0" />
+         <?php echo JHtml::_('form.token'); ?>
         <input type = "hidden" name = "view" value = "virtuemartcruces"  /> 
 
 </form>
