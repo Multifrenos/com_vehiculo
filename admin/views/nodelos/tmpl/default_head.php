57207cc7b8f1057dc1c9002daafc548cedb0ca77
Antonio-Multifrenos <multifrenos.es@gmail.com>
2017-11-07 13:14:57 +0100

    - Me mostraba una notice el log: Notice: Undefined variable: listDirn in /home/antonio/www/multipiezas/administrator/components/com_vehiculo/views/nodelos/tmpl/default_head.php on line 18
    Y funciona igualmente el orden, por eso lo quito...

--- admin/views/nodelos/tmpl/default_head.php
+++ admin/views/nodelos/tmpl/default_head.php
@@ -8,14 +8,14 @@ defined('_JEXEC') or die('Restricted Access');
 			<?php echo JHtml::_('grid.checkall'); ?>
         </th>
         <th width="5">
-			<?php echo JHtml::_('searchtools.sort', 'COM_VEHICULO_NODELOS_HEADING_ID', 'mo.id', $listDirn, $listOrder); ?>
+			<?php echo JHtml::_('searchtools.sort', 'COM_VEHICULO_NODELOS_HEADING_ID', 'mo.id'); ?>
         </th>
         <th width="10">
-			<?php echo JHtml::_('searchtools.sort', 'COM_VEHICULO_NODELOS_HEADING_MARCA', 'ma.nombre', $listDirn, $listOrder); ?>
+			<?php echo JHtml::_('searchtools.sort', 'COM_VEHICULO_NODELOS_HEADING_MARCA', 'ma.nombre'); ?>
     
         </th>
         <th width="10">
-			<?php echo JHtml::_('searchtools.sort', 'COM_VEHICULO_NODELOS_HEADING_NOMBREMODELO', 'mo.nombre', $listDirn, $listOrder); ?>
+			<?php echo JHtml::_('searchtools.sort', 'COM_VEHICULO_NODELOS_HEADING_NOMBREMODELO', 'mo.nombre'); ?>
 
                 <?php //echo JText::_('COM_VEHICULO_NODELOS_HEADING_NOMBREMODELO'); ?>
         </th>
