97612ab9197493fcc023ea05f1aff0c9eaf3eac6
Ricardo Oficina <ricardolau6@gmail.com>
2017-11-13 20:52:47 +0100

    Ahora si,,, ya funciona correctamente el campo versiones. 	modified:   admin/models/fields/versiones.php

--- admin/models/fields/versiones.php
+++ admin/models/fields/versiones.php
@@ -30,20 +30,44 @@ class JFormFieldVersiones extends JFormFieldList {
 	protected function getOptions()       
                 
 		{                
-		$db = JFactory::getDBO();
-		$query = $db->getQuery(true)
-                        ->select('id as value, nombre as text')
-                        ->from('#__vehiculo_versiones');
-		$db->setQuery($query);                
-                
-		try
-		{
-			$options = $db->loadObjectList();                             
+		
+		$db             = JFactory::getDBO();                                                             
+		$options        = array ();                
+		
+		// Get Marca from form or filter form
+		$fModelo         = $this->form->getField ('idModelo'); 
+		if (empty($fModelo)) {                        
+				$idModelo = $this->form->getField ('idModelo', 'filter')->value;
+		} else {
+				$idModelo = $this->form->getField ('idModelo')->value;
+		}       
+		
+		// Get the Marca associated to the selected model  
+		if (! empty ($idModelo)) {
+											  
+				$query = $db->getQuery(true)
+						->select('id as value, nombre as text')
+						->from('#__vehiculo_versiones')
+						->where ('idModelo = ' . (int) $idModelo);
+
+				$db->setQuery($query);                
+
+				try
+				{
+					$options = $db->loadObjectList();                             
+				}
+				catch (RuntimeException $e)
+				{
+					JError::raiseWarning(500, $e->getMessage);
+				}                
+				
 		}
-		catch (RuntimeException $e)
-		{
-			JError::raiseWarning(500, $e->getMessage);
-		}                
+		
+		
+		
+		           
+                
+		
                 		
 		return array_merge(parent::getOptions(), $options);
 	}
