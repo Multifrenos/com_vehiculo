fbafa0db00a6cf3d71e93d071dfb5b0212dcdb50
Antonio-Multifrenos <multifrenos.es@gmail.com>
2017-12-05 17:33:28 +0100

    - Creo funcion view.hmtl para obtener los datos vehiculo y producto. - Se que no esta bien... :-) 	modified:   admin/views/virtuemartcruce/tmpl/edit.php 	modified:   admin/views/virtuemartcruce/view.html.php

--- admin/views/virtuemartcruce/tmpl/edit.php
+++ admin/views/virtuemartcruce/tmpl/edit.php
@@ -12,20 +12,71 @@ JFactory::getDocument()->addScriptDeclaration("
 			}
 		};
 ");
-
+//~ echo '<pre>';
+//~ print_r($this->item);
+//~ echo '</pre>';
+$item = $this->item;
 ?>
-<form action="<?php echo JRoute::_('index.php?option=com_vehiculo&layout=edit&id='.(int) $this->item->id); ?>"
-      method="post" name="adminForm" id="virtuemartcruce-form" class="form-validate">
-        <fieldset class="adminform">
-                <legend><?php echo JText::_( 'Cruce de vehiculo con producto de virtuemart' ); ?></legend>
-                <ul class="adminformlist">
-<?php foreach($this->form->getFieldset() as $field): ?>
-                        <li><?php echo $field->label;echo $field->input;?></li>
-<?php endforeach; ?>
-                </ul>
-        </fieldset>
-        <div>
-                <input type="hidden" name="task" value="virtuemartcruce.edit" />
-                <?php echo JHtml::_('form.token'); ?>
-        </div>
-</form>
+<div class="row-fluid">
+<div class="span6">
+	<form action="<?php echo JRoute::_('index.php?option=com_vehiculo&layout=edit&id='.(int) $this->item->id); ?>"
+		  method="post" name="adminForm" id="virtuemartcruce-form" class="form-validate">
+			<fieldset class="adminform">
+					<legend><?php echo JText::_( 'Cruce de vehiculo con producto de virtuemart' ); ?></legend>
+					<ul class="adminformlist">
+			<?php foreach($this->form->getFieldset() as $field): ?>
+							<li><?php echo $field->label;echo $field->input;?></li>
+			<?php endforeach; ?>
+					</ul>
+			</fieldset>
+			<div>
+					<input type="hidden" name="task" value="virtuemartcruce.edit" />
+					<?php echo JHtml::_('form.token'); ?>
+			</div>
+	</form>
+</div>
+<div class="span6">
+	<h2><?php echo $item->get('id').' ID CRUCE -- '.$item->get('recambio_id').' ID Recambio(Tpv) -- '.$item->get('fecha_actualizacion').' Actualizado';?> </h2>
+	<h3> Coche seleccionado - ID Version :<?php echo $item->get('version_vehiculo_id');?>	</h3>
+	<?php
+	$vehiculo = $item->vehiculo;
+	$html = '<table class="table"><theader><th></th><th>Cilindrada</th><th>Cv/KW</th><th>NºCilindros</th><th>Inicio Fabricación</th><th>Fin Fabricación</th></theader>'
+			.'<tbody><tr>'
+			.'<td>'
+			.'<span title="'.'" class=" glyphicon glyphicon-info-sign"></span> '
+			.$item->get('virtuemart_product_id').' '.$vehiculo->marca.' '.$vehiculo->modelo.' '.$vehiculo->nombre
+			
+			.'</td>'
+			.'<td>'.$vehiculo->cm3.'</td>'
+			.'<td>'.$vehiculo->cv.'cv / '.$vehiculo->kw.'Kw</td>'
+			.'<td>'.$vehiculo->ncilindros.'</td>'
+
+			.'<td>'.$vehiculo->fecha_inicial.'</td>'
+			.'<td>'.$vehiculo->fecha_final.'</td>'
+			.'</tr></tbody>'
+			.'</table>';
+		echo '<div class="alert alert-info alert-dismissable">';
+			echo $html;
+		echo '</div>';
+	?>
+		
+	<h3> Producto seleccionado - ID de virtuemart: <?php echo $item->get('virtuemart_product_id');?>	</h3>
+	<?php
+	$producto = $item->producto;
+	$html = '<table class="table"><theader><th>idVirtuemart</th><th>SKU</th><th>Descripcion Producto</th><th>Precio</th><th>Fecha Ultima Modificacion</th></theader>'
+			.'<tbody><tr>'
+			.'<td>'
+			.$item->get('virtuemart_product_id')
+			.'</td>'
+			.'<td>'.$producto->product_sku.'</td>'
+			.'<td>'.$producto->articulo_name.'</td>'
+			.'<td>'.$producto->product_price.'</td>'
+			.'<td>'.$producto->modified_on.'</td>'
+			.'</tr></tbody>'
+			.'</table>';
+		echo '<div class="alert alert-info alert-dismissable">';
+			echo $html;
+		echo '</div>';
+	?>
+</div>
+</div>
--- admin/views/virtuemartcruce/view.html.php
+++ admin/views/virtuemartcruce/view.html.php
@@ -21,16 +21,20 @@ class VehiculoViewVirtuemartcruce extends JViewLegacy
 		// get the Data
 		$form = $this->get('Form');
 		$item = $this->get('Item');
-
+		//~ $marcarYmodelo = $this->get('MarcaModelo');
 		// Check for errors.
 		if (count($errors = $this->get('Errors'))) 
 		{
 			JError::raiseError(500, implode('<br />', $errors));
 			return false;
 		}
+		$itemNuevo = $this->getMasDatos($item);
+		//~ echo '<pre>';
+		//~ print_r($itemNuevo);
+		//~ echo '</pre>';
 		// Assign the Data
 		$this->form = $form;
-		$this->item = $item;
+		$this->item = $itemNuevo;
 
 		// Set the toolbar
 		$this->addToolBar();
@@ -50,4 +54,42 @@ class VehiculoViewVirtuemartcruce extends JViewLegacy
 		JToolBarHelper::save('virtuemartcruce.save');
 		JToolBarHelper::cancel('virtuemartcruce.cancel', $isNew ? 'JTOOLBAR_CANCEL' : 'JTOOLBAR_CLOSE');
 	}
+	
+	public function getMasDatos($item){
+			// Buscamos de vehiculo.
+			 $db = JFactory::getDBO();
+			 $id_version = $item->version_vehiculo_id;
+                $query = $db->getQuery(true);
+                //~ // Seleccione algunos campos
+                $query = $db->getQuery(true)
+                        ->select('v.id,ma.nombre as marca,mo.nombre as modelo,v.nombre,v.cm3,v.kw,v.ncilindros,v.cv,v.fecha_inicial,v.fecha_final')
+                        ->from('#__vehiculo_versiones as v')
+                        ->join('left', '#__vehiculo_modelos as mo ON (v.idModelo = mo.id)')
+                        ->join('left', '#__vehiculo_marcas as ma ON (v.idMarca = ma.id)')
+						->where('v.id = ' . $id_version);
+						
+				$db->setQuery($query);
+				$result = $db->loadObjectList();
+			// Añado datos de vehiculo a item.
+			$item->def('vehiculo',$result[0]);
+			// Buscamos datos de producto.
+			$db = JFactory::getDBO();
+			 $id_producto = $item->virtuemart_product_id;
+                $query = $db->getQuery(true);
+                //~ // Seleccione algunos campos
+                $query = $db->getQuery(true)
+                        ->select('c.virtuemart_product_id as idVirtuemart,CAST( c.product_sku as CHAR(18))as product_sku,cr.product_name as articulo_name,d.product_price, c.created_on as fecha_creado, c.modified_on')
+                        ->from('#__virtuemart_products as c')
+                        ->join('LEFT','#__virtuemart_products_es_es as cr ON (c.virtuemart_product_id = cr.virtuemart_product_id)')
+                        ->join('left', '#__virtuemart_product_prices as d on (c.virtuemart_product_id = d.virtuemart_product_id) ')
+						->where('c.virtuemart_product_id = ' . $id_producto);
+						
+				$db->setQuery($query);
+				$result = $db->loadObjectList();		
+				// Añado datos de vehiculo a item.
+				$item->def('producto',$result[0]);
+			
+			return $item;
+		
+		}
 }
